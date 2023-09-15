c06d45688670a21d53ede5fae55bee1d6def4fd8
adama <adhambek.otabekov.post@gmail.com>
2023-09-15 15:12:43 +0200

    minor

--- src/plug/utils/picky/install_deps.py
+++ src/plug/utils/picky/install_deps.py
@@ -1,8 +1,19 @@
 import os
+import threading
+import subprocess
 
 def installDeps(path):
 
     r=os.path.join(path, "requirements.txt")
     if os.path.exists(r):
         cmd=' '.join(['pip', 'install', '-r', r])
-        os.popen(cmd)
+        thread=runCMD(cmd)
+        thread.join()
+
+def runCMD(cmd):
+
+    def run(): subprocess.call(cmd)
+
+    thread=threading.Thread(target=run)
+    thread.start()
+    return thread
