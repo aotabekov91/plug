0ad8c4eed7034894c2bf8ddb40fc19b03f7e24d2
adam <adam@myworld.net>
2023-09-15 19:53:46 +0200

    minor

--- src/plug/utils/picky/install_deps.py
+++ src/plug/utils/picky/install_deps.py
@@ -1,19 +1,22 @@
 import os
-import threading
-import subprocess
+from pip._internal import main as pipmain
 
 def installDeps(path):
 
+    reqs=getReqs(path)
+    installReqs(reqs)
+
+def getReqs(path):
+
     r=os.path.join(path, "requirements.txt")
+    reqs=[]
     if os.path.exists(r):
-        cmd=' '.join(['pip', 'install', '-r', r])
-        thread=runCMD(cmd)
-        thread.join()
+        with open(r, 'r') as f:
+            for i in f.readlines():
+                reqs+=[i.strip('\n')]
+    return reqs
 
-def runCMD(cmd):
+def installReqs(reqs):
 
-    def run(): subprocess.call(cmd)
-
-    thread=threading.Thread(target=run)
-    thread.start()
-    return thread
+    for req in reqs:
+        pipmain(['install', req])
--- tests/test_install_deps/test_install_deps.py
+++ tests/test_install_deps/test_install_deps.py
@@ -4,14 +4,23 @@ from pathlib import Path
 
 from plug.utils.picky.install_deps import (
         installDeps,
+        # getReqs,
+        installReqs,
         )
+
+from pip._internal import main as pipmain
+
+@pytest.skip
+def test_new():
+
+    assert pipmain(['install', 'ankipulator @ git+https://github.com/aotabekov91/ankipulator@main'])
 
 @pytest.fixture
 def path():
 
     return Path(__file__).parent/'data/card'
 
-def test_installDeps(path):
+def test_getReqs(path):
 
     assert os.path.exists(path)
-    installDeps(path)
+    # assert getReqs(path)
