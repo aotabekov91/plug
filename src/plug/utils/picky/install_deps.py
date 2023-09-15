7d62ae472a32c8001931b3a8a8375feec5522030
adama <adhambek.otabekov.post@gmail.com>
2023-09-15 14:43:00 +0200

    minor

--- src/plug/utils/picky/install_deps.py
+++ src/plug/utils/picky/install_deps.py
@@ -5,4 +5,7 @@ def installDeps(ppath, cpath):
     r=os.path.join(cpath, "requirements.txt")
     print(r)
     if os.path.exists(r):
-        os.popen(['pip', 'install', '-r', r])
+        with open(r, 'r') as f:
+            for l in f.readlines():
+                req=l.strip('\n')
+                os.popen(['pip', 'install', '-r', req])
