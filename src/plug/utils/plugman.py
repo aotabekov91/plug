c1772747c8199ddb36d9c1836e50b26c10735fd6
adam <adam@myworld.net>
2023-09-15 22:02:29 +0200

    minor

--- src/plug/utils/plugman.py
+++ src/plug/utils/plugman.py
@@ -50,14 +50,15 @@ class Plugman:
         plugs=[]
         for name, folder in self.picky.rtp.items():
             sys.path.insert(0, folder)
-            # try:
 
-            m=importlib.import_module(name)
-            if hasattr(m, 'get_plug_class'):
-                plugs+=[m.get_plug_class()]
+            try:
 
-            # except ModuleNotFoundError:
-            #     print(f'Module {name} not found')
+                m=importlib.import_module(name)
+                if hasattr(m, 'get_plug_class'):
+                    plugs+=[m.get_plug_class()]
+
+            except ModuleNotFoundError:
+                print(f'Module {name} not found')
 
         self.loadPlugs(plugs)
         self.set('normal')
