54f2fc25bf7ac82e6f0787e9af86322c88e7402f
adam <adam@myworld.net>
2023-09-17 21:41:28 +0200

    minor

---
+++ src/plug/qt/plugs/trees/file_tree/__init__.py
@@ -0,0 +1,3 @@
+from .main import FileBrowser
+
+def get_plug_class(): return FileBrowser
---
+++ src/plug/qt/plugs/trees/file_tree/main.py
@@ -0,0 +1,85 @@
+import os
+from PyQt5.QtWidgets import QFileSystemModel
+
+from plug.qt.utils import register
+from plug.qt.plugs.trees.base import TreePlug
+
+class FileBrowser(TreePlug):
+
+    def __init__(self, 
+                 leader='.', 
+                 position='left',
+                 mode_keys={'command': 'f'}, 
+                 **kwargs):
+
+        super().__init__(
+                leader=leader,
+                position=position,
+                mode_keys=mode_keys,
+                **kwargs)
+        self.setPath()
+        self.app.plugman.plugsLoaded.connect(
+                self.on_plugsLoaded)
+
+    def on_plugsLoaded(self, plugs):
+
+        execlist=plugs.get('ExecList', None)
+        if execlist:
+            execlist.setArgOptions(
+                'openFile', 'path', 'path')
+
+    def getPath(self, index=None):
+
+        tree=self.ui.main.tree
+        if not index: 
+            index=tree.currentIndex()
+        if index:
+            model=tree.model()
+            path=model.filePath(index)
+            if os.path.exists(path): 
+                return path
+
+    def setPath(self, path=None):
+
+        tree=self.ui.main.tree
+        tree.setModel(QFileSystemModel())
+        if path is None: 
+            path = os.path.abspath('.')
+        tree.model().setRootPath(path)
+        index=tree.model().index(path)
+        tree.setRootIndex(index)
+
+        for i in range(1, 4): 
+            self.ui.main.tree.hideColumn(i)
+
+    @register(key='t', modes=['command'])
+    def toggle(self): super().toggle()
+
+    @register(modes=['exec'])
+    def openFile(self, 
+                 path, 
+                 how=None, 
+                 focus=True):
+
+        self.open(path, how, focus)
+
+    @register('<leader>o')
+    def open(self, 
+             path=None, 
+             how=None, 
+             focus=False):
+
+        if not path:
+            tree=self.ui.main.tree
+            index=tree.currentIndex()
+            path=self.getPath(index)
+        print(path)
+        if path:
+            if os.path.isdir(path): 
+                tree.expand(tree.currentIndex())
+            else:
+                self.app.window.main.open(
+                        path, 
+                        how=how, 
+                        focus=focus)
+            super().open(how, focus)
