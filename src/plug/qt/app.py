4f274f200d272af583f3969d090e104e96ca6b1c
adam <adam@myworld.net>
2023-09-17 12:52:10 +0200

    minor

--- src/plug/qt/app.py
+++ src/plug/qt/app.py
@@ -1,20 +1,18 @@
-import sys
 from PyQt5 import QtWidgets
 
 from gizmo.ui import StackWindow
 
 from plug.qt.plug import Plug
-from plug.qt.utils import Plugman
 
 class PlugApp(Plug):
 
     def __init__(self, *args, argv=[], **kwargs):
 
-        self.app=QtWidgets.QApplication()
+        self.app=QtWidgets.QApplication([])
 
         super(PlugApp, self).__init__(
                 *args,
-                argv=argv,
+                app=self.app,
                 **kwargs,
                 )
 
@@ -27,28 +25,4 @@ class PlugApp(Plug):
                 display_class, 
                 view_class)
 
-    def setup(self):
 
-        super().setup()
-        self.setPlugman(plugman=Plugman)
-
-    def initialize(self):
-
-        self.plugman.loadPicks()
-
-    def setName(self):
-
-        super().setName()
-        self.setApplicationName(self.name)
-
-    def run(self):
-
-        super().run()
-        if hasattr(self, 'window'): 
-            self.window.show()
-        sys.exit(self.exec_())
-
-    def exit(self): 
-
-        super().exit()
-        sys.exit()
