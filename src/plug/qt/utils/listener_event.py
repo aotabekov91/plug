4d51ce9b7392c7a4fe9ae7dfdd1ca0c90b6eeb84
adam <adam@myworld.net>
2023-09-17 20:36:20 +0200

    minor

--- src/plug/qt/utils/listener_event.py
+++ src/plug/qt/utils/listener_event.py
@@ -2,6 +2,13 @@ from gizmo.utils import EventListener as Base
 
 class EventListener(Base):
 
+    def toggleMode(self, mode):
+
+        if mode==self.obj:
+            self.delistenWanted.emit()
+        else:
+            self.modeWanted.emit(mode)
+
     def checkLeader(self, event):
 
         if self.app:
@@ -10,9 +17,12 @@ class EventListener(Base):
                 f=getattr(m, 'checkLeader', None)
                 if not f: continue 
                 if m.checkLeader(event, (self.pressed,)):
-                    if m==self.obj:
-                        self.delistenWanted.emit()
-                    else:
-                        self.modeWanted.emit(m)
+                    self.timer.stop()
+                    self.timer.timeout.disconnect()
+                    func=lambda: self.toggleMode(m)
+                    self.timer.timeout.connect(func)
+                    self.timer.start(self.wait_run)
                     return True
         return super().checkLeader(event)
+
+
