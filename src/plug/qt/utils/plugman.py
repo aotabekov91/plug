e50c30258ccd4a4c041a2155ef512b8692b57a4b
adam <adam@myworld.net>
2023-09-22 20:35:04 +0200

    minor

--- src/plug/qt/utils/plugman.py
+++ src/plug/qt/utils/plugman.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtCore
 
+from gizmo.utils import register
 from plug.utils import Plugman as Base
 
 class Plugman(Base, QtCore.QObject):
@@ -57,3 +58,19 @@ class Plugman(Base, QtCore.QObject):
 
         super().register(plug, actions)
         self.actionsRegistered.emit(plug, actions)
+
+    @register(modes=['exec'])
+    def installPicks(self): 
+        super().installPicks()
+
+    @register(modes=['exec'])
+    def installRequirements(self):
+        super().installRequirements()
+
+    @register(modes=['exec'])
+    def updatePicks(self): 
+        super().updatePicks()
+
+    @register(modes=['exec'])
+    def cleanupPicks(self): 
+        super().cleanupPicks()
